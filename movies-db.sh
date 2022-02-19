d8775ab43cee4fd6e2e697a7962bc4ad3b3b8cd7
charan379 <charanteja379@gmail.com>
2022-02-19 10:35:20 +0000

    new db backup

---
+++ movies-db.sh
@@ -0,0 +1,19 @@
+#go to folder
+
+cd /media/psbv/sqlbackup/movies/movies_db_backup
+
+#backup movies db
+
+python3 backup.py singlerun localhost movies_php4dvd root swordFish@379 . noarchive fasle
+
+#git add
+
+git add .
+
+# commit
+
+git commit -m "new db backup"
+
+# push
+
+git push https://charan379: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@github.com/charan379/movies_db_backup.git --all

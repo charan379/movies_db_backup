a8309bfe5ad3fbe17ec8380aeabe301f6521488f
charan379 <charanteja379@gmail.com>
2022-02-20 08:11:31 +0000

    new db backup

--- bcmail.py
+++ bcmail.py
@@ -14,6 +14,75 @@ from email import encoders
 
 runparam = sys.argv[1]
 
+#Mailer
+
+def sendmail():
+    # SMTP Mail
+
+    # Python code to illustrate Sending mail with attachments
+    # from your Gmail account
+
+    fromaddr = "yandrapaticharanteja@gmail.com"
+    toaddr = "charanteja379@gmail.com"
+
+    # instance of MIMEMultipart
+    msg = MIMEMultipart()
+
+    # storing the senders email address
+    msg['From'] = fromaddr
+
+    # storing the receivers email address
+    msg['To'] = toaddr
+
+    # storing the subject
+    msg['Subject'] = db_name+'_'+ backupdate
+
+    # string to store the body of the mail
+    body = "Movies Library Backup on " + backupdate
+
+    # attach the body with the msg instance
+    msg.attach(MIMEText(body, 'plain'))
+
+    # open the file to be sent
+    # my path is pipes.quote(fullpath) + "/" + db + ".sql"
+    filename = db + ".sql"
+    #attachment = open("/mnt/g/"+ filename, "rb")
+    attachment = open(pipes.quote(fullpath) + "/" + filename, "rb")
+
+    # instance of MIMEBase and named as p
+    p = MIMEBase('application', 'octet-stream')
+
+    # To change the payload into encoded form
+    p.set_payload((attachment).read())
+
+    # encode into base64
+    encoders.encode_base64(p)
+
+    p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+
+    # attach the instance 'p' to instance 'msg'
+    msg.attach(p)
+
+    # creates SMTP session
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+
+    # start TLS for security
+    s.starttls()
+
+    # Authentication
+    s.login(fromaddr, "swordFish@379")
+
+    # Converts the Multipart msg into a string
+    text = msg.as_string()
+
+    # sending the mail
+    s.sendmail(fromaddr, toaddr, text)
+
+    # terminating the session
+    s.quit()
+
+#Mailer end
+
 def backuper (db_host, db_name, db_user, db_pass, backup_path, arc_arg, hide_pass):
     print ("============================================================================")
     print ("Using current settings: ")
@@ -111,68 +180,4 @@ app_branch = "staging"
 initialize()
 
 
-def sendmail():
-    # SMTP Mail
-
-    # Python code to illustrate Sending mail with attachments
-    # from your Gmail account
-
-    fromaddr = "yandrapaticharanteja@gmail.com"
-    toaddr = "charanteja379@gmail.com"
-
-    # instance of MIMEMultipart
-    msg = MIMEMultipart()
-
-    # storing the senders email address
-    msg['From'] = fromaddr
-
-    # storing the receivers email address
-    msg['To'] = toaddr
-
-    # storing the subject
-    msg['Subject'] = db_name+'_'+ backupdate
-
-    # string to store the body of the mail
-    body = "Movies Library Backup on " + backupdate
-
-    # attach the body with the msg instance
-    msg.attach(MIMEText(body, 'plain'))
-
-    # open the file to be sent
-    # my path is pipes.quote(fullpath) + "/" + db + ".sql"
-    filename = db + ".sql"
-    #attachment = open("/mnt/g/"+ filename, "rb")
-    attachment = open(pipes.quote(fullpath) + "/" + filename, "rb")
-
-    # instance of MIMEBase and named as p
-    p = MIMEBase('application', 'octet-stream')
-
-    # To change the payload into encoded form
-    p.set_payload((attachment).read())
-
-    # encode into base64
-    encoders.encode_base64(p)
-
-    p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
-
-    # attach the instance 'p' to instance 'msg'
-    msg.attach(p)
-
-    # creates SMTP session
-    s = smtplib.SMTP('smtp.gmail.com', 587)
-
-    # start TLS for security
-    s.starttls()
-
-    # Authentication
-    s.login(fromaddr, "swordFish@379")
-
-    # Converts the Multipart msg into a string
-    text = msg.as_string()
-
-    # sending the mail
-    s.sendmail(fromaddr, toaddr, text)
-
-    # terminating the session
-    s.quit()
 

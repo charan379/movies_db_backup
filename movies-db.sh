0e227d68ec2f50f6eb5cefbe93c3d9e34a10594b
charan379 <charanteja379@gmail.com>
2022-02-20 07:55:55 +0000

    new db backup

--- movies-db.sh
+++ movies-db.sh
@@ -1,10 +1,11 @@
+
 #go to folder
 
 cd /media/psbv/sqlbackup/movies/movies_db_backup
 
 #backup movies db
 
-python3 backup.py singlerun localhost movies_php4dvd root swordFish@379 . noarchive fasle
+python3 bcmail.py singlerun localhost movies_php4dvd root swordFish@379 . noarchive fasle
 
 #git add
 
